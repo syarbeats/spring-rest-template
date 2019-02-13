e4cb4f4ca8da3f90059d92bca33fdc3fd2989bcf
Syarif Hidayat <Syarif_H657@mitrais.com>
2019-02-13 13:01:09 +0700

    CRUD refactoring

--- src/main/java/com/learning/spring/spring_rest_template/App.java
+++ src/main/java/com/learning/spring/spring_rest_template/App.java
@@ -23,24 +23,60 @@ public class App
 	
     public static void main( String[] args ) 
     {
-    	JsonNode root = null;
-    	RestTemplate restTemplate = new RestTemplate();
-    	ResponseEntity<String> response;
-    	ObjectMapper mapper;
-    	String getEmployeeResourceUrl = GET_EMPLOYEE_LIST;
-    	String getEmployeeByIdResourceUrl = GET_EMPLOYEE_BY_ID;
-       
+    	App app = new App();
+    	
     	/**
     	 * GET DATA FROM REST API - OBJECT MODE RESPONSE
     	 * **/
-    	//ResponseEntity<String> response = restTemplate.getForEntity(getEmployeeResourceUrl+"?page=1&size=3", String.class);
-    	response = restTemplate.getForEntity(getEmployeeByIdResourceUrl+"/202", String.class);
-    	Employee employee = restTemplate.getForObject(getEmployeeByIdResourceUrl+"/202", Employee.class);
+    	Employee employee = app.getEmployeeById(808);
+    	System.out.println("Employe Name: "+ employee.getName()+" Salary: "+employee.getSalary());
+    	
+    	String name = app.getEmployeNameById(808);
+    	System.out.println("Employee name: "+name);
+    	
+    	/**
+    	 * GET DATA FROM REST API - ARRAY OF OBJECT MODE RESPONSE
+    	 * **/
+    	List<Employee> empList = app.getEmployeeList(2, 3);
+    	
+    	for(Employee emp:empList) {
+			System.out.println("Name: "+emp.getName()+" Salary: "+emp.getSalary());
+		}
+    	
+    
+    	/**
+    	 * POST OBJECT DATA TO REST API ENDPOINT
+    	 * **/
+    	app.addEmployee(new Employee(708, "Chapo El Guzman", 150000));
+    	
+  
+    	
+    }
+    
+    /**
+	 * GET DATA FROM REST API - OBJECT MODE RESPONSE
+	 * **/
+    public Employee getEmployeeById(int id) {
+    	
+    	RestTemplate restTemplate = new RestTemplate();
+        Employee employee = restTemplate.getForObject(GET_EMPLOYEE_BY_ID+"/"+id, Employee.class);
+    
+    	return employee;
+    }
+    
+    /**
+	 * GET DATA FROM REST API - OBJECT MODE RESPONSE
+	 * **/
+    public String getEmployeNameById(int id) {
+    	ObjectMapper mapper;
+    	RestTemplate restTemplate = new RestTemplate();
+    	JsonNode root = null;
+    	
+    	ResponseEntity<String> response = restTemplate.getForEntity(GET_EMPLOYEE_BY_ID+"/"+id, String.class);
     	
     	System.out.println("Response Status Code: "+response.getStatusCode());
     	System.out.println("Response Body: "+response.getBody());
-    	System.out.println("Employee Name:"+employee.getName()+" Salary: "+employee.getSalary());
-       
+    	
     	mapper =  new ObjectMapper();
        
     	try {
@@ -51,41 +87,51 @@ public class App
     	}
     	
     	JsonNode name = root.path("name");
-    	System.out.println("Name: "+name);
-       
-    	/**
-    	 * GET DATA FROM REST API - ARRAY OF OBJECT MODE RESPONSE
-    	 * **/
-    	response = restTemplate.getForEntity(getEmployeeResourceUrl+"?page=1&size=4", String.class);
+    	
+    	return name.asText();
+    	
+    }
+    
+    /**
+	 * GET DATA FROM REST API - ARRAY OF OBJECT MODE RESPONSE
+	 * **/
+    public List<Employee> getEmployeeList(int page, int size){
+    	
+    	RestTemplate restTemplate = new RestTemplate();
+    	ResponseEntity<String> response = restTemplate.getForEntity(GET_EMPLOYEE_LIST+"?page="+page+"&size="+size, String.class);
     	
     	System.out.println("Response Status Code: "+response.getStatusCode());
     	System.out.println("Response Body: "+response.getBody());
        
-    	mapper =  new ObjectMapper();
+    	ObjectMapper mapper =  new ObjectMapper();
        
     	try {
     		List<Employee> empList = mapper.readValue(response.getBody(), new TypeReference<List<Employee>>() {});
-    		
-    		for(Employee emp:empList) {
-    			System.out.println("Name: "+emp.getName()+" Salary: "+emp.getSalary());
-    		}
-    		
+    		return empList;
     	}catch(Exception e) {
     		e.printStackTrace();
     	}
     	
-    	/**
-    	 * POST OBJECT DATA TO REST API ENDPOINT
-    	 * **/
-    	HttpEntity<Employee> request = new HttpEntity<>(new Employee(802, "Schrodinger", 65000));
+    	return null;
+    }
+    
+    /**
+	 * POST OBJECT DATA TO REST API ENDPOINT
+	 * **/
+    public Employee addEmployee(Employee emp) {
+    	
+    	RestTemplate restTemplate = new RestTemplate();
+    	HttpEntity<Employee> request = new HttpEntity<>(emp);
     	
     	try {
-    		Employee empl = restTemplate.postForObject(ADD_EMPLOYEE, request, employee.getClass());
+    		Employee empl = restTemplate.postForObject(ADD_EMPLOYEE, request, Employee.class);
     		System.out.println("Employee Data "+empl.getName()+" Salary: "+empl.getSalary()+" has been inserted successfully...");
+    		return empl;
     	}catch(Exception e) {
     		e.printStackTrace();
     	}
     	
-    	
+    	return null;
     }
+    
 }
